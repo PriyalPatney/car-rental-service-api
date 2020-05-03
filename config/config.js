dc603b2881e5057cc2a3d0087a9a5c1b8f3be2e8
Priyal Patney <priyal_patney@hotmail.com>
2020-05-03 14:26:32 +0530

    mongo update

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ module.exports = {
     development: {
         port: process.env.PORT || 3000,
         dbPath: 'mongodb://localhost:27017/car-renting-data'
-        //  dbPath: 'mongodb://car:carsoftoday@cluster0-hyqgi.mongodb.net/test?retryWrites=true&w=majority'
+        
     },
     production: {
         port:process.env.PORT,
         dbPath: process.env.DATABASEURL
-        // dbPath: 'mongodb+srv://car:carsoftoday@cluster0-hyqgi.mongodb.net/test?retryWrites=true&w=majority'
+
     }
 };
