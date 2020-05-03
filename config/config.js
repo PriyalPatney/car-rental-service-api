4c84b2269807e15701ad0a63173689713c6f9a29
Priyal Patney <priyal_patney@hotmail.com>
2020-05-03 12:58:23 +0530

    mongo added

--- config/config.js
+++ config/config.js
@@ -3,5 +3,8 @@ module.exports = {
         port: process.env.PORT || 3000,
         dbPath: 'mongodb://localhost:27017/car-renting-data'
     },
-    production: {}
+    production: {
+        port:3000,
+        dbPath: 'mongodb+srv://car:carsoftoday@cluster0-hyqgi.mongodb.net/test?retryWrites=true&w=majority'
+    }
 };
